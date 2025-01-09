f973bba8c56b51c128710d2bf3985fa1c99b0262
vegris <1cheaterok1@gmail.com>
2025-01-09 20:31:14 +0300

    Rename batch to queue in Pachka.State

--- lib/pachka/state.ex
+++ lib/pachka/state.ex
@@ -4,28 +4,28 @@ defmodule Pachka.State do
   @type t :: %__MODULE__{
           config: Pachka.Config.t(),
           state: __MODULE__.Idle.t() | __MODULE__.Exporting.t() | __MODULE__.RetryBackoff.t(),
-          batch: [Pachka.message()],
-          batch_length: non_neg_integer()
+          queue: [Pachka.message()],
+          queue_length: non_neg_integer()
         }
 
   @enforce_keys ~w[config state]a
-  defstruct @enforce_keys ++ [batch: [], batch_length: 0]
+  defstruct @enforce_keys ++ [queue: [], queue_length: 0]
 
-  defguard is_empty(state) when state.batch_length == 0
-  defguard is_batch_ready(state) when state.batch_length >= state.config.max_batch_size
-  defguard is_full(state) when state.batch_length >= state.config.critical_batch_size
+  defguard is_empty(state) when state.queue_length == 0
+  defguard is_batch_ready(state) when state.queue_length >= state.config.max_batch_size
+  defguard is_full(state) when state.queue_length >= state.config.critical_batch_size
 
   @spec add_message(t(), Pachka.message()) :: t()
   def add_message(%__MODULE__{} = state, message) do
-    %__MODULE__{state | batch: [message | state.batch], batch_length: state.batch_length + 1}
+    %__MODULE__{state | queue: [message | state.queue], queue_length: state.queue_length + 1}
   end
 
   @spec take_batch(t()) :: {[Pachka.message()], t()}
   def take_batch(%__MODULE__{} = state) do
-    rest_length = max(state.batch_length - state.config.max_batch_size, 0)
-    {rest, batch} = Enum.split(state.batch, rest_length)
+    rest_length = max(state.queue_length - state.config.max_batch_size, 0)
+    {rest, batch} = Enum.split(state.queue, rest_length)
 
-    state = %__MODULE__{state | batch: rest, batch_length: rest_length}
+    state = %__MODULE__{state | queue: rest, queue_length: rest_length}
 
     {Enum.reverse(batch), state}
   end
