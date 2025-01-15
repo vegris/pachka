810816e99a32efd08ad85cdd5c786341867515a9
vegris <1cheaterok1@gmail.com>
2025-01-15 11:57:11 +0300

    Relax Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pachka.MixProject do
     [
       app: :pachka,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
